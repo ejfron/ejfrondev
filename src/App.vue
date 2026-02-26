d64e8e2cf31bd6e8cb63d60dd9e271d83da7631b
EDGAR JUGADO <edgarjugado0505@gmail.com>
2026-02-26 21:43:52 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -237,7 +237,7 @@ border: 1px solid rgba(255, 255, 255, 0.3);
    --border-color: #f4f4f4e8;
     --line-span: #10c010;
       --bubbles: #51fc9025;
-         --profile: linear-gradient(to bottom,#ffffff 0%,#78cc6d 100%);
+         --profile: linear-gradient(to bottom,#01010125 0%,#ffff 100%);
 }
 
 
